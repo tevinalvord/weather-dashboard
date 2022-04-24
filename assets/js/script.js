7ec592825b4bf27f3f30de738b69c6da4653f865
tevinalvord <tevin.alvord@gmail.com>
2022-04-24 11:37:35 -0600

    can search any city to see current day weather stats

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,60 +1,72 @@
-// var currentWeatherData = function(lat, lon) {
-//     var apiUrl = "https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + lon + "&exclude={part}&appid=0edbb4eebb59bb25285ea86cab81a271"
-
-//     fetch(apiUrl);
-// }
-
-// currentWeatherData(40, -111);
-
-// var getCityLatLon = function(city, state, country) {
-//     var apiUrl = "http://api.openweathermap.org/geo/1.0/direct?q=" + city + "," + state + "," + country + "&limit=&appid=0edbb4eebb59bb25285ea86cab81a271";
-
-//     fetch(apiUrl).then(function(response) {
-//         if (response.ok) {
-//             response.json().then(function(data) {
-//                 var lat = data[0].lat;
-//                 var lon = data[0].lon;
-//                 currentWeatherData(lat, lon);
-//             })
-//         } else {
-//             alert("Incorrect city, state, or country input");
-//         }
-//     });
-// };
-
-// getCityLatLon("provo", "utah", "us")
-
 var formEl = document.querySelector("#form");
 var cityInputEl = document.querySelector("#search-city");
-var weatherInfoEL = document.querySelector("#weather-info");
+var weatherInfoEl = document.querySelector("#current-weather");
+var today = new Date();
+var date = (today.getMonth()+1) + "/" + today.getDate() + "/" + today.getFullYear();
 
-var displayWeatherData = function(weatherData) {
-    var currentTemp = weatherData.current.temp;
+var forcastWeatherData = function() {
+
+};
+
+var displayCurrentWeatherData = function(weatherData) {
+
+
+};
+
+var currentWeatherData = function(weatherData) {
+    var currentIcon = weatherData.current.weather[0].icon;
+    weatherInfoEl.textContent = "";    
 
     var weatherDataEl = document.createElement("div");
     weatherDataEl.classList = "weather-data-div";
 
     var cityNameEl = document.createElement("h1");
-    cityNameEl.textContent = cityInputEl.value;
+    var cityName = cityInputEl.value;
+    cityInputEl.value = "";
+    cityNameEl.textContent = cityName[0].toUpperCase() + cityName.substring(1) + " (" + date + ")";
+    cityNameEl.classList = "section-title";
+    var currentIconEl = document.createElement("img");
+    currentIconEl.classList = "current-icon"
+    currentIconEl.setAttribute("src", "https://openweathermap.org/img/wn/" + currentIcon + "@2x.png");
+    cityNameEl.appendChild(currentIconEl);
     weatherDataEl.appendChild(cityNameEl);
 
     var cityTemp = document.createElement("p");
-    cityTemp.textContent = "Temp:" + currentTemp;
+    var currentTemp = weatherData.current.temp;
+    cityTemp.textContent = "Temp: " + currentTemp + "°F";
+    cityTemp.classList = "current-weather-info";
     weatherDataEl.appendChild(cityTemp);
 
-    weatherInfoEL.appendChild(weatherDataEl);
+    var cityWind = document.createElement("p");
+    var currentWind = weatherData.current.wind_speed;
+    cityWind.textContent = "Wind: " + currentWind + " MPH";
+    cityWind.classList = "current-weather-info";
+    weatherDataEl.appendChild(cityWind);
 
+    var cityHumidity = document.createElement("p");
+    var currentHumidity = weatherData.current.humidity;
+    cityHumidity.textContent = "Humidity: " + currentHumidity + " %";
+    cityHumidity.classList = "current-weather-info";
+    weatherDataEl.appendChild(cityHumidity);
 
+    var cityUvIndex = document.createElement("p");
+    var currentUvIndex = weatherData.current.uvi;
+    // cityUvIndex.textContent = "UV Index: " + currentUvIndex;
+    cityUvIndex.innerHTML = "<span>UV Index: </span><span class='uv-index'>" + currentUvIndex + "</span>"
+    cityUvIndex.classList = "current-weather-info";
+    weatherDataEl.appendChild(cityUvIndex);
+
+    weatherInfoEl.classList.add("current-weather-border");
+    weatherInfoEl.appendChild(weatherDataEl);
     console.log(weatherData);
 };
 
-var currentWeatherData = function(lat, lon) {
+var getAllWeatherData = function(lat, lon) {
     var apiUrl = "https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + lon + "&exclude={part}&units=imperial&appid=0edbb4eebb59bb25285ea86cab81a271"
-
     fetch(apiUrl).then(function(response) {
         if (response.ok) {
             response.json().then(function(data) {
-                displayWeatherData(data);
+                currentWeatherData(data);
             })
         } else {
             alert("Something went wrong. Try again.")
@@ -70,16 +82,13 @@ var getCityGeo = function(city) {
             response.json().then(function(data) {
                 var lat = data[0].lat;
                 var lon = data[0].lon;
-                currentWeatherData(lat, lon);
+                getAllWeatherData(lat, lon);
             })
         } else {
             alert("Something went wrong. Try again.");
         }
     });
 };
-
-// testing
-// getCityGeo("Provo");
 
 var formSubmitHandler = function(event) {
     event.preventDefault();
@@ -88,7 +97,6 @@ var formSubmitHandler = function(event) {
 
     if (city) {
         getCityGeo(city);
-        cityInputEl.value = "";
     } else {
         alert("Please enter a city name");
     }
